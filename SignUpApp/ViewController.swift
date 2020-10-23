2831a5c333b699721d28efd94fb565a571f64664
aleneweaver11 <aleneweaver11@gmail.com>
2020-10-23 09:13:23 -0400

    Initial Commit

---
+++ SignUpApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SignUpApp
+//
+//  Created by  on 10/23/20.
+//  Copyright © 2020 AubreyApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
